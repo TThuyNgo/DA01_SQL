fd3a21f68f6b8604f76b89b83ba7b9a613d2a709
TThuyNgo <160410189+TThuyNgo@users.noreply.github.com>
2024-02-26 09:19:01 +0100

    B7.sql

--- B7.sql
+++ B7.sql
@@ -4,3 +4,9 @@ FROM pharmacy_sales
 GROUP BY manufacturer
 ORDER BY sum(total_sales) DESC,manufacturer;
 --ex4
+SELECT EXTRACT(month FROM submit_date) AS month, product_id, 
+ROUND(AVG(stars),2) AS avg_stars	 
+FROM reviews
+GROUP BY EXTRACT(month FROM submit_date),product_id
+ORDER BY EXTRACT(month FROM submit_date),product_id;
+--ex5
