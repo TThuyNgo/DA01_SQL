b2a6f03d3422a13355aad3bdfd70abfb4d0c03a5
TThuyNgo <160410189+TThuyNgo@users.noreply.github.com>
2024-03-14 18:34:27 +0100

    Project2.2.sql

--- Project2.2.sql
+++ Project2.2.sql
@@ -1,3 +1,4 @@
+/*Tạo metric*/
 WITH rev_table AS (
   SELECT
     year_month,
@@ -38,3 +39,36 @@ SELECT
 FROM rev_table r
 JOIN cost_table c ON c.year_month = r.year_month
 ORDER BY r.year_month
+
+/*cohort analysis*/
+WITH cohort AS (
+SELECT
+user_id,order_id,
+FORMAT_DATE('%Y-%m', first_purchase_date) AS cohort_date,
+created_at,
+(EXTRACT(YEAR FROM created_at)-EXTRACT(YEAR FROM first_purchase_date))*12 + (EXTRACT(month FROM created_at) -EXTRACT(month FROM first_purchase_date)) +1 AS index
+FROM(
+SELECT 
+    user_id,
+    order_id,
+    MIN(created_at) OVER (PARTITION BY user_id) AS first_purchase_date,
+    created_at
+FROM 
+    bigquery-public-data.thelook_ecommerce.orders
+GROUP BY 
+    user_id, created_at,order_id) a)
+, prep AS(SELECT
+cohort_date, index,count(distinct user_id) AS cohort_size, count(order_id) total_orders
+FROM cohort
+GROUP BY cohort_date, index)
+SELECT cohort_date,
+SUM(CASE WHEN index = 1 THEN cohort_size ELSE 0 END) AS m1,
+SUM(CASE WHEN index = 2 THEN cohort_size ELSE 0 END) AS m2,
+SUM(CASE WHEN index = 3 THEN cohort_size ELSE 0 END) AS m3,
+
+FROM prep
+GROUP BY cohort_date
+ORDER BY  cohort_date
+
+/*visualization link*/
+https://docs.google.com/spreadsheets/d/1251nYVxiJbEsjHf5svN4dqhjJtPsruHx1vyiuwV39lw/edit#gid=935230167
